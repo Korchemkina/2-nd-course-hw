9a2f6d5cd4ae38c07698b5972ac0cb1caa4606ee
Korchemkina <fedorova.av@mail.ru>
2025-11-08 18:23:17 +0300

    Выполнены задания по JS Массивы

--- games.js
+++ games.js
@@ -1,33 +1,61 @@
-const guessTheNumber = () => {
-    alert('Угадай число');
+// const guessTheNumber = () => {
+//     alert('Угадай число');
 
-    let randomNumber = Math.floor(Math.random() * 100) + 1;
-    console.log(randomNumber);
-    let userInput, userNumber;
+//     let randomNumber = Math.floor(Math.random() * 100) + 1;
+//     console.log(randomNumber);
+//     let userInput, userNumber;
 
-    for (;;) {
-        userInput = prompt('Введите число от 1 до 100');
+//     for (;;) {
+//         userInput = prompt('Введите число от 1 до 100');
 
-        if (userInput === null) {
-            break;
-        } 
+//         if (userInput === null) {
+//             break;
+//         } 
 
-        userNumber = Number(userInput);
+//         userNumber = Number(userInput);
 
-        if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
-            alert('Введено неверное значение');
-            continue;
+//         if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
+//             alert('Вы ввели неверное значение');
+//             continue;
+//         }
+
+//         if (randomNumber === userNumber) {
+//             alert('Вы угадали!');
+//             break;
+//         } else {
+//             alert(`введеное число ${userNumber > randomNumber ? 'больше' : 'меньше'} загаданного`);
+//         }    
+//     }
+
+//     if (confirm('Играем ещё раз?')) {
+//         guessTheNumber();
+//     }
+// }
+
+const simpleArithmetic = () => {
+    alert('Простая арифметика');
+}
+
+        function startGame() {
+            const operators = ['+', '-', '*', '/'];
+            const num1 = getRandomInt(1, 20);
+            const num2 = getRandomInt(1, 20);
+            const operator = operators[getRandomInt(0, operators.length)];
+
+            let question = `${num1} ${operator} ${num2}`;
+            let userAnswer = prompt(`Решите задачу: ${question}`);
+
+            // Оценка правильного ответа
+            let correctAnswer = eval(question);
+
+            // Проверка и вывод результата
+            if (parseFloat(userAnswer) === correctAnswer) {
+                document.getElementById('result').innerText = 'Верный ответ!';
+            } else {
+                document.getElementById('result').innerText = `Ошибка! Правильный ответ: ${correctAnswer}`;
+            }
         }
 
-        if (randomNumber === userNumber) {
-            alert('Угадал!');
-            break;
-        } else {
-            alert(`введеное число ${userNumber > randomNumber ? 'больше' : 'меньше'} загаданного`);
-        }    
-    }
-
-    if (confirm('Играем ещё раз!')) {
-        guessTheNumber();
-    }
-}
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
