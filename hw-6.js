b580968a0f8f174afa2aa2cac8082d31c7b504a6
Korchemkina <fedorova.av@mail.ru>
2025-11-23 21:37:37 +0300

    Выполнены задания по JS ДЗ-7 Встроенные объекты

--- hw-6.js
+++ hw-6.js
@@ -1,11 +1,11 @@
 console.log('\n%c Задание 1','color:green'); 
 const arr = [1, 5, 4, 10, 0, 3];
-    for (let i = 0; i < arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
     
     console.log(arr[i]);
         if (arr[i] === 10) {
             break;
-    }
+        }
 }
 
 console.log('\n%c Задание 2','color:green'); 
